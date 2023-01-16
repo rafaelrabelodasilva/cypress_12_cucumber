e2c31ddad0888eddd54f210d2f7c479f3c2981dc
Rafael Rabelo da Silva <rafaelrabelodasilva@outlook.com>
2023-01-16 19:48:08 -0300

    Ajustando código cadastro_usuario

--- cypress/e2e/step_definitions/cadastro_usuario.js
+++ cypress/e2e/step_definitions/cadastro_usuario.js
@@ -3,14 +3,23 @@ import {Given, When, Then} from "@badeball/cypress-cucumber-preprocessor";
 var faker = require('faker-br');
 
 Given('que acesso a página de cadastro do site Publicazo', () => {
-    cy.intercept('GET', 'http://publicazo.insprak.com/').as('paginaInicial')
+    cy.intercept('GET', 'http://publicazo.insprak.com/')
+    .as('paginaInicial')
+
     cy.visit('/')
     cy.wait('@paginaInicial')
-    cy.get(':nth-child(4) > a').should('have.text', 'Entrar')
 
-    cy.intercept('GET','http://publicazo.insprak.com/sign_up').as('telaCadastro')
-    cy.get(':nth-child(3) > a').click()
+    cy.get(':nth-child(4) > a')
+    .should('have.text', 'Entrar')
+
+    cy.intercept('GET','http://publicazo.insprak.com/sign_up')
+    .as('telaCadastro')
+
+    cy.get(':nth-child(3) > a')
+    .click()
+
     cy.wait('@telaCadastro')
+
     cy.get('h2').should('have.text', 'Cadastre-se')
 })
 
@@ -19,19 +28,29 @@ Then('devo ver a mensagem {string}', (mensagem) => {
 })
 
 When('submeto o formulário de cadastro com dados válidos', () => {
-    cy.get('#user_fullname').type(faker.name.findName())
-    cy.get('#user_email').type(faker.internet.email())
-    cy.get('#user_password').type(Cypress.env('senha'))
-    cy.get('#user_password_confirmation').type(Cypress.env('senha'))
+    cy.get('#user_fullname')
+    .type(faker.name.findName())
+
+    cy.get('#user_email')
+    .type(faker.internet.email())
+
+    cy.get('#user_password')
+    .type(Cypress.env('senha'))
+
+    cy.get('#user_password_confirmation')
+    .type(Cypress.env('senha'))
     cy.get('.btn').click()
 })
 
 When('não preencho nenhum campo', () => {
-    cy.get('.btn').click()
+    cy.get('.btn')
+    .click()
 })
 
 Then('devo ver as mensagens {string}, {string} e {string}', (mensagem1, mensagem2, mensagem3) => {
-    cy.get('.toast').should('be.visible').and('contain', mensagem1, mensagem2, mensagem3)
+    cy.get('.toast')
+    .should('be.visible')
+    .and('contain', mensagem1, mensagem2, mensagem3)
 })
 
 When('submeto o formulário com senhas divergentes', () => {
